9781d3ed2e86195307abf1d63dc45edea5713a86
franco sacca <francosaccamp@gmail.com>
2024-09-12 19:43:28 -0300

    for

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hola a todos")
+
+
+const h1 = document.querySelector("h1");
+h1.innerText = "Productos";
+
+// let array = [];
+// for (let i = 1 ; i < 10 ; i++) {
+//   array.push(`
+//     `)
+// }
+let cardsArray = [];
+for (let i = 1; i < 10; i++) {
+    cardsArray.push(` 
+        <div class="card">
+            <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+                <p class="card-text">Tarjeta numero ${i}.</p>
+            </div>
+        </div>
+    `); //batiks para el contenido
+    let container = document.querySelector(".container"); //seleccionamos para poder agregar la card.
+  }
+  console.log("Intento " + cardsArray + "para ver si funciona");
+  document.querySelector(".container").innerHTML = cardsArray.join('');
